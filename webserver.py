85cec52d54c4b03738a9d037dd2e12773f6b1f19
danielvicentini <34514421+danielvicentini@users.noreply.github.com>
2019-03-11 22:18:23 -0300

    Add files via upload

--- webserver.py
+++ webserver.py
@@ -1,4 +1,3 @@
-
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import logging
 import json 
@@ -16,8 +15,6 @@ from webexteamssdk import WebexTeamsAPI
 # Sergio Polizer
 # Daniel Vicentini
 
-
-
 #########################################################
 ## VAR FIXAS
 
@@ -32,16 +29,20 @@ botmail="infobot@sparkbot.io"
 #########################################################
 ## FUNCOES
 
+### WEBEX TEAMS
 
 def CriaWebhook(webhook_name,webhook_url):
+
 
 	# Cria Webhook para receber msg via POST
     # Avisa Teams para gerar hooks para mensagems criadas somente
 
+	# Webhook para msgs
     api.webhooks.create(webhook_name,webhook_url,"messages","created")
-
+	# Webhook para nova sala criada - boas vindas
+    api.webhooks.create(webhook_name+"-new",webhook_url,"rooms","created")
+	
     return
-
 
 def webexME():
 	# detalhes sobre mim
@@ -189,7 +190,172 @@ def webexmsgAll(msg):
 	return
 
 
+### OPERATIONS INSIGHTS
+
+def OpiCategorySearch(textosearch):
+
+	# Faz uma pesquisa no Operation Insights por Categorias existentes
+
+	# Parte 1 - pede Token para o OPI
+	url = "https://opinsights.cisco.com/api/am/v1/auth/license/accesstoken"
+	headers = {'Content-type': "application/json" , 'Authorization':'JWT eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5hbnRJZCI6MjExLCJ0eXBlIjoiODQzMDc0YjAtMzIwYS0xMWU4LTg0YTctMzkzZWNiNTViY2Q5IiwidXNlck5hbWUiOiJmbGNvcnJlYUBjaXNjby5jb20iLCJpYXQiOjE1NTA4NjI5NTQsImp0aSI6IjQ2YjRiNTIwLTM2ZDYtMTFlOS04OTRmLTI3YTM3MDQ2NWExNCJ9.sNEnOPr45NXNRgx9AyEqU4x9xrt8ra-miRSX2rfH9K4'   }
+
+	# response = valor HTTP (nao usado ainda) e conteudo e' o conteudo de fato, covertendo em json
+	response = requests.request("GET", url, headers=headers)
+	conteudo=json.loads(response.content)
+	# resultado do token
+	token='JWT ' + str(conteudo['token'])
+
+	# Parte 2 - Consulta assets usando o Token
+	url = "https://opinsights.cisco.com/api/am/v1/entities/access/categories"
+	headers = {     'Content-type': "application/json" , 'Authorization': ''+token  }
+
+	# response = valor HTTP (nao usado ainda) e conteudo e' o conteudo de fato, covertendo em json
+	response = requests.request("GET", url, headers=headers)
+	Jdata=json.loads(response.content)
+
+	# Laco que Faz a pesquisa baseado no grupo do dispositivo da base acima
+
+	# Permite procurar tudo caso esta keyword seja usada
+	if textosearch == "tudo":
+		textosearch = ""
+
+	resultado = ""
+	
+	count = 0
+
+	for items in Jdata:
+
+	  msg=""
+
+	  if textosearch in str(items['department']['name']).lower() or textosearch in str(items['name']).lower():
+           
+		   #constroi saida de texto
+			 msg=msg+str("Nome:"+str(items['name'])+". Departamento: "+str(items['department']['name'])+"\n")
+			 count=count+1
+			 resultado = resultado + msg
+		
+	resultado = resultado + "\n"+str(count)+" Categorias Encontradas"	
+
+	return resultado
+
+
+def OpiAssetDetail(textosearch):
+	
+	# Pesquisa detalhes de um asset
+
+	# Parte 1 - pede Token para o OPI
+	url = "https://opinsights.cisco.com/api/am/v1/auth/license/accesstoken"
+	headers = {'Content-type': "application/json" , 'Authorization':'JWT eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5hbnRJZCI6MjExLCJ0eXBlIjoiODQzMDc0YjAtMzIwYS0xMWU4LTg0YTctMzkzZWNiNTViY2Q5IiwidXNlck5hbWUiOiJmbGNvcnJlYUBjaXNjby5jb20iLCJpYXQiOjE1NTA4NjI5NTQsImp0aSI6IjQ2YjRiNTIwLTM2ZDYtMTFlOS04OTRmLTI3YTM3MDQ2NWExNCJ9.sNEnOPr45NXNRgx9AyEqU4x9xrt8ra-miRSX2rfH9K4'   }
+
+	# response = valor HTTP (nao usado ainda) e conteudo e' o conteudo de fato, covertendo em json
+	response = requests.request("GET", url, headers=headers)
+	conteudo=json.loads(response.content)
+	# resultado do token
+	token='JWT ' + str(conteudo['token'])
+
+	# Parte 2 - Consulta assets usando o Token
+	url = "https://opinsights.cisco.com/api/am/v1/entities/access/assets"
+	headers = {     'Content-type': "application/json" , 'Authorization': ''+token  }
+
+	# response = valor HTTP (nao usado ainda) e conteudo e' o conteudo de fato, covertendo em json
+	response = requests.request("GET", url, headers=headers)
+	Jdata=json.loads(response.content)
+
+	# Laco que Faz a pesquisa baseado no grupo do dispositivo da base acima
+
+	resultado=""
+	
+	for items in Jdata:	
+		msg=""		
+		if textosearch in str(items['serial']).lower():
+			# Caso positivo encontrado monta a resposta
+			msg=msg+str("Asset:"+str(items['serial'])+"\n")	
+			msg=msg+str("Serial: "+str(items['tags'][0]['serial'])+"\n")
+			msg=msg+str("Tipo: "+str(items['tags'][0]['type'])+"\n")
+			msg=msg+str("Categoria: "+str(items['category']['name'])+"\n")
+			msg=msg+str("Local:"+str(items['site']['name'])+"\n")
+			msg=msg+str("Departamento:"+str(items['department']['name'])+"\n")	
+			# Devolve a zona identificada pelo Insights, caso exista
+			try:
+				zona=str(items['location']['zones'][0]['name'])
+				msg=msg+str("Local: Sala "+str(zona)+"\n")
+			except:
+			  pass
+			  msg=msg+str("Local não encontrado\n")		
+			
+			resultado=resultado+str(msg)
+
+	if resultado=="":
+		resultado="Nenhum Asset encontrado"
+
+	return resultado
+
+
+
+def OpiFindAssets(textosearch):
+	# Faz uma pesquisa no Operation Insights
+
+	# Parte 1 - pede Token para o OPI
+	url = "https://opinsights.cisco.com/api/am/v1/auth/license/accesstoken"
+	headers = {'Content-type': "application/json" , 'Authorization':'JWT eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5hbnRJZCI6MjExLCJ0eXBlIjoiODQzMDc0YjAtMzIwYS0xMWU4LTg0YTctMzkzZWNiNTViY2Q5IiwidXNlck5hbWUiOiJmbGNvcnJlYUBjaXNjby5jb20iLCJpYXQiOjE1NTA4NjI5NTQsImp0aSI6IjQ2YjRiNTIwLTM2ZDYtMTFlOS04OTRmLTI3YTM3MDQ2NWExNCJ9.sNEnOPr45NXNRgx9AyEqU4x9xrt8ra-miRSX2rfH9K4'   }
+
+	# response = valor HTTP (nao usado ainda) e conteudo e' o conteudo de fato, covertendo em json
+	response = requests.request("GET", url, headers=headers)
+	conteudo=json.loads(response.content)
+	# resultado do token
+	token='JWT ' + str(conteudo['token'])
+
+	# Parte 2 - Consulta assets usando o Token
+	url = "https://opinsights.cisco.com/api/am/v1/entities/access/assets"
+	headers = {     'Content-type': "application/json" , 'Authorization': ''+token  }
+
+	# response = valor HTTP (nao usado ainda) e conteudo e' o conteudo de fato, covertendo em json
+	response = requests.request("GET", url, headers=headers)
+	Jdata=json.loads(response.content)
+
+
+	# Laco que Faz a pesquisa baseado no grupo do dispositivo da base acima
+
+	# Permite procurar tudo caso esta keyword seja usada
+	if textosearch == "tudo":
+		textosearch = ""
+
+	resultado = ""
+	
+	count = 0
+
+	for items in Jdata:
+
+		msg=""
+		 
+		if textosearch in str(items['tags'][0]['type']).lower() or textosearch in str(items['category']['name']).lower() or textosearch in str(items['serial']).lower():
+		
+			count = count +1
+						
+			# Caso encontrado monta a resposta
+			msg=msg+str(str(count)+")Asset:"+str(items['serial'])+" Categoria: "+str(items['category']['name'])+" . ")	
+			
+			try:
+			 zona=str(items['location']['zones'][0]['name'])
+			 msg=msg+str("Local: Sala "+str(zona)+"\n")
+			except:
+
+			 pass
+			 msg=msg+str("Local não encontrado\n")		
+			
+
+		
+		resultado = resultado + str(msg)
+
+		
+	resultado = resultado + "\n"+str(count)+" Assets Encontrados"	
+
+	return resultado
+
+
 def procura(textosearch):
+	# DEPRECADO - NAO USADO MAIS
 	# Faz uma pesquisa no Operation Insights
 
 	# Parte 1 - pede Token para o OPI
@@ -237,7 +403,7 @@ def procura(textosearch):
 			msg=msg+str("tipo: "+str(items['tags'][0]['type'])+"\n")
 			msg=msg+str("categoria: "+str(items['category']['name'])+"\n")
 
-			count = count +1
+			count=count+1
 		
 		resultado = resultado + str(msg)
 
@@ -266,16 +432,29 @@ def webextalk(msg_id):
     # Splita para encontrar detalhes dos parametros
     sp=box.split(" ")
     box=sp[0]
+	# converte para minusculas
+    box=box.lower()
 
     # chamadas de acordo com os parametros
-    if box == "help":
-        msg="Chatops Teams 1.0\nComandos disponiveis:\nhelp: esta ajuda\nprocura <item>: Procurar Asset\n"
-        msg=msg+str("userid <email>: Identifica ID do usuario\nroomid <nome da sala>: Identifica ID da sala\nsalas: lista salas que pertenco\nexit: sai")
+    if box == "ajuda" or box =="help":
+        msg="Chatops Teams 1.0\nComandos disponiveis:\nhelp: esta ajuda\nProcura <nome ou tudo>: Procurar local do Asset\nCategorias <nome ou tudo>: Lista Categorias cadastradas no OPI como nome identificado\nAsset <nome do asset>:apresenta detalhes do Asset\n"
+        msg=msg+str("userid <email>: Identifica ID do usuario\nroomid <nome da sala>: Identifica ID da sala\nsalas: lista salas que pertenco\n")
             
     # chama funcao para procurar OPI somente se ouver 2 parametros
     if box == "procura" and len(sp)>1:
         asset=sp[1]
-        msg = procura(asset)
+        msg = OpiFindAssets(asset)
+
+    # chama funcao para procurar OPI somente se ouver 2 parametros
+    if box == "categorias" and len(sp)>1:
+        categoria=sp[1]
+        msg = OpiCategorySearch(categoria)
+
+	# chama funcao para procurar OPI somente se ouver 2 parametros
+    if box == "asset" and len(sp)>1:
+        asset=sp[1]
+        msg = OpiAssetDetail(asset)
+ 
             
     # chamada funcao se houver no minimo 2 parametros
     if box == "userid" and len(sp)>1:
@@ -292,6 +471,8 @@ def webextalk(msg_id):
         
     # apos montagem da resposta em msg, envia a respectiva sala teams:
     webexmsgRoomviaID(idsala,msg)
+
+    return
 
 # RASCUNHOS ANTERIORES
 #########################################################
@@ -345,15 +526,18 @@ def webextalk(msg_id):
 #########################################################
 ## LOGICA COMECA AQUI
 
-
 # inicia programa
-
-
 # Site
 
-# Cria webhook para receber POSTs
-CriaWebhook(webhook_name,webhook_url)
-
+# Valida existencia de webhook
+webhookok=0
+webhook=api.webhooks.list()
+for b in webhook:
+	 if b.name==webhook_name:
+		 webhookok=1
+# Webhook nao encontrado. Cria novos.
+if webhookok==0:
+	CriaWebhook(webhook_name,webhook_url)
 
 # http server
 class S(BaseHTTPRequestHandler):
@@ -373,23 +557,43 @@ class S(BaseHTTPRequestHandler):
     def do_POST(self):
         content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
         post_data = self.rfile.read(content_length) # <--- Gets the data itself
-        logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
-                str(self.path), str(self.headers), post_data.decode('utf-8'))
+        logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n", str(self.path), str(self.headers), post_data.decode('utf-8'))
         self._set_response()
         self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
 
-        # Daniel Vicentini
-        # Aciona bot caso webhook seja identificado e mensagem postada nao seja do proprio bot
+        # Conteudo
         content=json.loads(post_data.decode('utf-8'))
-        if content['name']==webhook_name and content['data']['personEmail']!=botmail:
-            
-            # identifica id da mensagem
-            msg_id=(content['data']['id'])
-            # executa a logica conforme o pedido (interacao)
-            webextalk(msg_id)
 
+		#trata o código caso venha do OIP
+        try:
+            if content['rule']!="":
+                #identifica serial
+                asset=content['assets'][0]['serial']
+                #identifica msg
+                aviso=content['message']
+                #monta msg e manda para todas as salas webex onde robo esta presente
+                msg="Asset "+str(asset)+" informa: "+str(aviso)
+                webexmsgAll(msg)
+        except:
+            pass
+
+		# Msg de boas vindas para novas salas criadas Não esta' funcionando
+        #if content['name']==webhook_name+"-new" and content['data']['personEmail']!=botmail:
+        #    novasala=(content['data']['roomId'])
+        #    webexmsgRoomviaID(novasala,"Olá! Digite ajuda para conhecer as opções.")
+		
+		# resposta as perguntas que chegam para o robo
+        try:
+            if content['name']==webhook_name and content['data']['personEmail']!=botmail:
+                # identifica id da mensagem
+                msg_id=(content['data']['id'])
+                # executa a logica conforme o pedido (interacao)
+                webextalk(msg_id)
+        except:
+            pass
 
 def run(server_class=HTTPServer, handler_class=S, port=int(os.getenv('PORT',8080))):
+	# ou roda o server na porta 8080 ou pega a porta do ambiente (típico de PaaS)
     logging.basicConfig(level=logging.INFO)
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
